f1d8b3cef5d38d741e55d13d978aa757c2fc82dc
Katja Stefan <katja.stefan18@gmail.com>
2020-05-13 12:28:51 +0200

    Initial set up of weather application

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+    let long, lat;
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            // console.log(`position: ${position}`);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            console.log(`long = ${long}, lat = ${lat}`);
+            
+            
+        })
+    } else {
+        h1.textContent = 'Please enable your location tracker...'
+    }
+});
